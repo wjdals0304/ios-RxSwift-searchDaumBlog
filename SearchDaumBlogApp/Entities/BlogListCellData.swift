ae976e1b82bb077ac22d4b1055436d4dd2f0ae5b
jmkim <wjdals0304@gmail.com>
2022-04-21 11:52:15 +0900

    add: BlogListCellData file add

---
+++ SearchDaumBlogApp/Entities/BlogListCellData.swift
@@ -0,0 +1,16 @@
+//
+//  BlogListCellData.swift
+//  SearchDaumBlogApp
+//
+//  Created by 김정민 on 2022/04/21.
+//
+
+import Foundation
+
+
+struct BlogListCellData {
+    let thumbnailURL: URL?
+    let name: String?
+    let title: String?
+    let datetime: Date?
+}
