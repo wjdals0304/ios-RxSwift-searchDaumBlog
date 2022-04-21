07c24769bd308650ddaa53933502ecd7b2d7adc0
jmkim <wjdals0304@gmail.com>
2022-04-21 17:22:32 +0900

    add:  search blog api  추가

---
+++ SearchDaumBlogApp/Network/SearchNetwork.swift
@@ -0,0 +1,66 @@
+//
+//  SearchNetwork.swift
+//  SearchDaumBlogApp
+//
+//  Created by 김정민 on 2022/04/21.
+//
+
+import RxSwift
+import UIKit
+
+struct SearchBlogAPI {
+    static let scheme = "https"
+    static let host = "dapi.kakao.com"
+    static let path = "/v2/search/"
+    
+    func searchBlog(query: String) -> URLComponents {
+        var components = URLComponents()
+        components.scheme = SearchBlogAPI.scheme
+        components.host = SearchBlogAPI.host
+        components.path = SearchBlogAPI.path + "blog"
+        
+        components.queryItems = [
+            URLQueryItem(name: "query", value: query),
+            URLQueryItem(name: "size", value: "25")
+        ]
+        
+        return components
+    }
+}
+
+
+final class SearchBlogNetwork {
+    
+    private let session: URLSession
+    let api = SearchBlogAPI()
+    
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    func searchBlog(query:String) -> Single<Result<DKBlog,SearchNetworkError>> {
+        
+        guard let url = api.searchBlog(query: query).url else {
+            return .just(.failure(.invalidURL))
+        }
+        
+        let request = NSMutableURLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("KakaoAK 4e78ea35cffb481201121cd3d09455a6", forHTTPHeaderField: "Authorization")
+        
+        return session.rx.data(request: request as URLRequest)
+            .map { data in
+                do {
+                    let blogData = try JSONDecoder().decode(DKBlog.self, from: data)
+                    return .success(blogData)
+                } catch {
+                    return .failure(.invalidJSON)
+                }
+            }
+            .catch { _ in
+                .just(.failure(.networkError))
+            }
+            .asSingle()
+        
+    }
+}
