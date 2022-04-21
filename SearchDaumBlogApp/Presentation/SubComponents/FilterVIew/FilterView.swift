c50a433784de6f9b4ff46921422bbb04f0509ded
jmkim <wjdals0304@gmail.com>
2022-04-21 11:29:55 +0900

    add: tableView header view layout add

---
+++ SearchDaumBlogApp/Presentation/SubComponents/FilterVIew/FilterView.swift
@@ -0,0 +1,60 @@
+//
+//  FilterView.swift
+//  SearchDaumBlogApp
+//
+//  Created by 김정민 on 2022/04/21.
+//
+
+import RxSwift
+import RxCocoa
+import UIKit
+
+
+final class FilterView: UITableViewHeaderFooterView {
+    
+    let disposeBag = DisposeBag()
+    
+    let sortButton = UIButton()
+    let bottomBorder = UIView()
+    
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        
+        attribute()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func bind(_ viewModel: FilterViewModel) {
+        sortButton.rx.tap
+            .bind(to: viewModel.sortButtonTapped)
+            .disposed(by: disposeBag)
+    }
+    
+    private func attribute() {
+        sortButton.setImage(UIImage(systemName: "list.bullet"), for: .normal)
+        bottomBorder.backgroundColor = .lightGray
+    }
+    
+    private func layout() {
+        [sortButton, bottomBorder]
+            .forEach { addSubview($0) }
+        
+        sortButton.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(12)
+            $0.width.height.equalTo(28)
+        }
+        
+        bottomBorder.snp.makeConstraints {
+            $0.top.equalTo(sortButton.snp.bottom)
+            $0.leading.trailing.bottom.equalToSuperview()
+            $0.height.equalTo(0.5)
+        }
+    }
+    
+    
+}
