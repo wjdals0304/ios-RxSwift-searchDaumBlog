e6201833f52d78b9d31d97d9a5f3a5a13069c2e0
jmkim <wjdals0304@gmail.com>
2022-04-21 11:30:20 +0900

    add:  FilterViewModel file add

---
+++ SearchDaumBlogApp/Presentation/SubComponents/FilterVIew/FilterViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  FilterViewModel.swift
+//  SearchDaumBlogApp
+//
+//  Created by 김정민 on 2022/04/21.
+//
+
+import RxSwift
+import RxCocoa
+
+
+struct FilterViewModel {
+    
+    let sortButtonTapped = PublishRelay<Void>()
+    
+    
+}
