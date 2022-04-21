e627fa42cd7d2a4a1f97c2ceb7fdc7505c16fd30
jmkim <wjdals0304@gmail.com>
2022-04-21 17:21:17 +0900

    add: network error enum file add

---
+++ SearchDaumBlogApp/Entities/SearchNetworkError.swift
@@ -0,0 +1,24 @@
+//
+//  SearchNetworkError.swift
+//  SearchDaumBlogApp
+//
+//  Created by 김정민 on 2022/04/21.
+//
+
+import Foundation
+
+
+enum SearchNetworkError: Error {
+    case invalidURL
+    case invalidJSON
+    case networkError
+    
+    var message: String {
+        switch self {
+        case .invalidURL, .invalidJSON:
+            return "데이터를 불러올 수 없습니다."
+        case .networkError:
+            return "네트워크 상태를 확인해주세요."
+        }
+    }
+}
