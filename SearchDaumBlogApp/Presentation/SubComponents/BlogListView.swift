3bfde3c1a4ed12989b73d90d1e5a42983b5f4e8c
jmkim <wjdals0304@gmail.com>
2022-04-21 15:20:15 +0900

    add: BlogListView file add

---
+++ SearchDaumBlogApp/Presentation/SubComponents/BlogListView.swift
@@ -0,0 +1,60 @@
+//
+//  BlogListView.swift
+//  SearchDaumBlogApp
+//
+//  Created by 김정민 on 2022/04/21.
+//
+
+import RxSwift
+import RxCocoa
+import UIKit
+
+final class BlogListView: UITableView {
+    
+    let disposeBag = DisposeBag()
+    
+    let headerView = FilterView(
+        frame: CGRect(
+            origin: .zero,
+            size: CGSize(width: UIScreen.main.bounds.width, height: 50)
+        )
+    )
+    
+    override init(frame: CGRect, style: UITableView.Style) {
+        super.init(frame: frame, style: style)
+        
+        attribute()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    func bind(_ viewModel: BlogListViewModel) {
+        
+        headerView.bind(viewModel.filterViewModel)
+        
+        viewModel.cellData
+            .asDriver(onErrorJustReturn: [])
+            .drive(self.rx.items) { tv, row, data in
+                let index = IndexPath(row: row, section: 0)
+                let cell = tv.dequeueReusableCell(withIdentifier: "BlogListCell", for: index) as! BlogListCell
+                
+                cell.setData(data)
+                return cell
+            }.disposed(by: disposeBag)
+    }
+    
+    
+    
+
+    private func attribute() {
+        self.backgroundColor = .white
+        self.register(BlogListCell.self, forCellReuseIdentifier: "BlogListCell")
+        self.separatorStyle = .singleLine
+        self.rowHeight = 100
+        self.tableHeaderView = headerView
+    }
+    
+}
