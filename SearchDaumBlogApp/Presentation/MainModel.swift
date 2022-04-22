f9c96fcf0b25d9aeb9c07f2a071ea4ac45158685
jmkim <wjdals0304@gmail.com>
2022-04-22 10:31:54 +0900

    add: main network model file add

---
+++ SearchDaumBlogApp/Presentation/MainModel.swift
@@ -0,0 +1,64 @@
+//
+//  MainModel.swift
+//  SearchDaumBlogApp
+//
+//  Created by 김정민 on 2022/04/21.
+//
+
+import Foundation
+import RxSwift
+
+struct MainModel {
+    
+    let network = SearchBlogNetwork()
+    
+    func searchBlog(_ query: String) -> Single<Result<DKBlog,SearchNetworkError>> {
+        return network.searchBlog(query: query)
+    }
+    
+    func getBlogValue(_ result: Result<DKBlog,SearchNetworkError>) -> DKBlog? {
+        
+        guard case .success(let value) = result else {
+            return nil
+        }
+        return value
+        
+    }
+    
+    func getBlogError(_ result: Result<DKBlog, SearchNetworkError>) -> String? {
+        guard case .failure(let error) = result else {
+            return nil
+        }
+        return error.message
+    }
+    
+    func getBlogListCellData(_ value: DKBlog?) -> [BlogListCellData] {
+        guard let value = value else {
+            return []
+        }
+        
+        return value.documents
+            .map {
+                let thumbnailURL = URL(string: $0.thumbnail ?? "")
+                return BlogListCellData(
+                    thumbnailURL: thumbnailURL,
+                    name: $0.name,
+                    title: $0.title,
+                    datetime: $0.datetime
+                )
+            }
+    }
+    
+    func sort(by type: MainViewController.AlertAction, of data: [BlogListCellData]) -> [BlogListCellData] {
+        switch type {
+        case .title:
+            return data.sorted { $0.title ?? "" < $1.title ?? "" }
+        case .datetime:
+            return data.sorted { $0.datetime ?? Date() > $1.datetime ?? Date() }
+        case .cancel, .confirm:
+            return data
+        }
+    }
+    
+    
+}
